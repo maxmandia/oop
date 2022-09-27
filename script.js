87fae441306bb85eda654c38958ad423ab79b666
Max Mandia <mmandia511@gmail.com>
2022-09-27 19:50:23 -0400

    feat: allowed user to add book

--- script.js
+++ script.js
@@ -1,15 +1,52 @@
-let book = document.querySelector(".book");
-
-book.addEventListener("click", (e) => {
-  console.log(e.target.innerHTML);
-});
-
+let bookContainer = document.querySelector(".book-container");
+let addBook = document.querySelector("button");
 let myLibrary = ["the hobbit", "harry potter"];
+let main = document.querySelector("main");
 
-function Book() {
-  // the constructor...
+function Book(title) {
+  this.title = title;
+  this.addBook = () => {
+    const bookElement = document.createElement("p");
+    const node = document.createTextNode(title);
+    bookElement.appendChild(node);
+    bookContainer.appendChild(bookElement);
+  };
 }
 
-function addBookToLibrary() {
-  // do stuff here
-}
+// function addBookToLibrary() {
+//   myLibrary.forEach((book) => {
+//     const bookElement = document.createElement("p");
+//     const node = document.createTextNode(book);
+//     bookElement.appendChild(node);
+//     bookContainer.appendChild(bookElement);
+//   });
+// }
+
+// addBookToLibrary();
+addBook.addEventListener("click", () => {
+  const formElement = document.createElement("form");
+  const authorElement = document.createElement("label");
+  const authorInput = document.createElement("input");
+  const titleElement = document.createElement("label");
+  const titleInput = document.createElement("input");
+  const pagesElement = document.createElement("label");
+  const pagesInput = document.createElement("input");
+  const submitBTN = document.createElement("button");
+  titleElement.innerHTML = "title";
+  authorElement.innerHTML = "author";
+  pagesElement.innerHTML = "pages";
+  submitBTN.innerHTML = "add";
+  main.appendChild(formElement);
+  formElement.appendChild(titleElement);
+  formElement.appendChild(titleInput);
+  formElement.appendChild(authorElement);
+  formElement.appendChild(authorInput);
+  formElement.appendChild(pagesElement);
+  formElement.appendChild(pagesInput);
+  main.appendChild(submitBTN).addEventListener("click", (e) => {
+    e.preventDefault();
+    const myNewBook = new Book(authorInput.value);
+    console.log(myNewBook.title);
+    myNewBook.addBook();
+  });
+});
