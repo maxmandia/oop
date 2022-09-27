906b4b2af9a4a7fdc6522d812a72d2ae425386a4
Max Mandia <mmandia511@gmail.com>
2022-09-27 17:50:32 -0400

    saving work

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hey");
+let book = document.querySelector(".book");
+
+book.addEventListener("click", (e) => {
+  console.log(e.target.innerHTML);
+});
+
+let myLibrary = ["the hobbit", "harry potter"];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
